f252faf2d1d96ee2b45799c6bdd93b24b8fe293a
Brandow Figueira <brandow.cgrj@hotmail.com>
2022-06-27 19:12:21 -0300

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -2,7 +2,7 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(72) NOT NULL,
-  `token` varchar(255) NOT NULL
+  `token` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `usuarios` ADD PRIMARY KEY (`id`);
